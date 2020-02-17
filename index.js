b632ce4339006d3b4a1f647544dceb235868009d
joordy <jmfronik@gmail.com>
2020-02-17 14:56:20 +0100

    upload

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.get('/', function(req, res) {
 app.get('/matches', function(req, res) {
     res.sendFile(__dirname + '/public/matches.html')
 });
-
-app.get('/matches', function(req, res) {
-    res.sendFile(__dirname + '/public/matches.html')
-});
 app.get('/overview', function(req, res) {
     res.sendFile(__dirname + '/public/overview.html')
 });
@@ -24,4 +20,4 @@ app.get('/*', function(req, res) {
     res.sendFile(__dirname + '/public/404.html')
 });
 
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+app.listen(3000, () => console.log('App is listening on port 3000!'));

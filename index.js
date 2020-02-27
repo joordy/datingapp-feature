be1b433df4ce78bb3ea9d336d8f724a5c2349721
joordy <jmfronik@gmail.com>
2020-02-27 13:21:20 +0100

    routing los gedefinieerd

--- index.js
+++ index.js
@@ -6,40 +6,73 @@ const PORT = 3000;
 let urlEncodedParser = bodyParser.urlencoded({ extended: false })
 
 
-// Template setup
+// Template setup, en static files ophalen middleware. Volgorde maakt uit!!
 app.set('view engine', 'ejs');
-// Static file ophalen
+app.set('views', 'views');
 app.use(express.static('static'));
 
-// Routes Dynamic
-app.get('/', function(req, res) {
+// routing of EJS pages
+app.get('/', home);
+app.get('/matches', matches);
+app.get('/matchlist', matchlist);
+app.get('/profile', profile);
+app.post('/profile', urlEncodedParser, profilePost);
+app.get('/*', notfound);
+
+// functions for routing
+function home(req, res) {
     res.render('index');
-});
-app.get('/matches', function(req, res) {
+};
+
+function matches(req, res) {
     res.render('matches');
-});
-app.get('/matchlist', function(req, res) {
-    res.render('matchlist');
-});
-app.get('/profile', function(req, res) {
+};
+
+function matchlist(req, res) {
+    res.render('matches');
+};
+
+function profile(req, res) {
     res.render('profile', { qs: req.query });
-});
-// post method to succes page
-app.post('/profile', urlEncodedParser, function(req, res) {
+};
+
+function profilePost(req, res) {
     console.log(req.body);
     res.render('profile-succes', { data: req.body });
-});
-//
-app.get('/test/:name', function(req, res) {
-    // insert data from database( in this case, from object)
-    let data = { age: 22, job: 'Student', hobby: ['Eating', 'Drawing', 'Shopping'] };
-    // render everything in EJS.
-    res.render('test', { person: req.params.name, data: data });
-});
+};
 
-app.get('/*', function(req, res) {
+function notfound(req, res) {
     res.render('404');
-});
+};
+
+// Routes Dynamic
+// app.get('/', function(req, res) {
+//     res.render('index');
+// });
+// app.get('/matches', function(req, res) {
+//     res.render('matches');
+// });
+// app.get('/matchlist', function(req, res) {
+//     res.render('matchlist');
+// });
+// app.get('/profile', function(req, res) {
+//     res.render('profile', { qs: req.query });
+// });
+// // post method to succes page
+// app.post('/profile', urlEncodedParser, function(req, res) {
+//     console.log(req.body);
+//     res.render('profile-succes', { data: req.body });
+// });
+
+// app.get('/test/:name', function(req, res) {
+//     // insert data from database( in this case, from object)
+//     let data = { age: 22, job: 'Student', hobby: ['Eating', 'Drawing', 'Shopping'] };
+//     // render everything in EJS.
+//     res.render('test', { person: req.params.name, data: data });
+// });
+// app.get('/*', function(req, res) {
+//     res.render('404');
+// });
 
 // Server aanzetten
 app.listen(3000, () => console.log(`App is listening on ${PORT}!`));

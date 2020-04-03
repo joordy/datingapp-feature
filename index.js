4fdfaab84c820d811d033777957e5d85fd4df89d
joordy <jmfronik@gmail.com>
2020-04-03 15:03:51 +0200

    cleaned up code with some removing unnecessary packages and comments

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// require packages
+// require packages or files
 const express = require('express');
 const bodyParser = require('body-parser');
-const slug = require('slug');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const mongo = require('mongodb');
 const assert = require('assert');
 const routing = require('./routing/route.js');
-require("dotenv").config();
+require('dotenv').config();
 
 // Variables
 const app = express();
@@ -19,13 +18,15 @@ app.set('views', 'view-ejs');
 app.use(express.static('static'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: true,
     resave: false,
     secure: true
-}));
-app.use('/', routing); // using routing module
+  })
+);
+app.use('/', routing);
 
 // Server deploying on https://localhost:4000.
 app.listen(PORT, () => console.log(`App is listening on ${PORT}!`));
